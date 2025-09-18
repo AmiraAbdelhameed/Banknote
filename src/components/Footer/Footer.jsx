77a90d17a6247002d3a4be84e78fe0779a38e29d
unknown <amerakhames42@gmail.com>
2025-09-18 22:33:15 +0300

    add footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='container section-continer pt-16 bg-black '>
+      <div className="upper-section grid grid-cols-3 md:grid-cols-5">
+<div className="image">
+    <img src="./images/footerLogo.png" alt="" srcset="" />
+</div>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Outlet } from 'react-router'
 import Navbar from '../components/Navbar/Navbar'
+import Footer from '../components/Footer/Footer'
 const Layout = () => {
   return (
     <>
       <Navbar />
       <Outlet />
+      <Footer />
     </>
   )
 }

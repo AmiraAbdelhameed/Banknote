8b5c268aaae4d58b2b90a4a9a3e69c7b0678fa1d
unknown <amerakhames42@gmail.com>
2025-09-19 17:34:28 +0300

    edit hero section

--- src/components/Hero/Slider.jsx
+++ src/components/Hero/Slider.jsx
@@ -1,65 +1,61 @@
-import React,{useState} from 'react'
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Pagination } from 'swiper/modules';
-import { Carousel } from 'primereact/carousel';
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Autoplay } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
 
 const Slider = () => {
-  let images=[
-    './images/heroSection/slider/slider1.jpg',
-    './images/heroSection/slider/slider2.jpg',
-    './images/heroSection/slider/slider3.jpg',
-    './images/heroSection/slider/slider4.jpg',
-    './images/heroSection/slider/slider5.jpg',
-    './images/heroSection/slider/slider6.jpg',
-    './images/heroSection/slider/slider7.jpg',
-    './images/heroSection/slider/slider8.jpg',
-    './images/heroSection/slider/slider9.jpg',
-   
-  ]
-   const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const images = [
+    "./images/heroSection/slider/slider1.jpg",
+    "./images/heroSection/slider/slider2.jpg",
+    "./images/heroSection/slider/slider3.jpg",
+    "./images/heroSection/slider/slider4.jpg",
+    "./images/heroSection/slider/slider5.jpg",
+    "./images/heroSection/slider/slider6.jpg",
+    "./images/heroSection/slider/slider7.jpg",
+    "./images/heroSection/slider/slider8.jpg",
+    "./images/heroSection/slider/slider9.jpg",
+  ];
 
-  const goToNext = () => {
-    setCurrentImageIndex((prevIndex) => 
-      (prevIndex === images.length - 1 ? 0 : prevIndex + 1)
-    );
-  };
-
-  const goToPrevious = () => {
-    setCurrentImageIndex((prevIndex) => 
-      (prevIndex === 0 ? images.length - 1 : prevIndex - 1)
-    );
-  };
   return (
-    <>
-    <div className='relative overflow-hidden '>
+    <div className="slider bg-[url(/images/heroSection/background.png)] bg-no-repeat relative overflow-hidden py-10">
 
-    <figure className='overflow-hidden'>
-     <img src="./images/heroSection/background.png" alt="" srcset=""  />
-    </figure>
-    <div className="absolute top-0">
-        <img src="./images/heroSection/offer.png" alt="" srcset=""  className='z-10'/>
+      <div className="absolute top-0 left-0">
+        <img
+          src="./images/heroSection/offer.png"
+          alt="offer"
+          className="z-10 relative"
+        />
+      </div>
+
+      <Swiper
+        modules={[Pagination]}
+        pagination={{ clickable: true }}
+        centeredSlides={true}
+        loop={true}
+        slidesPerView={5} 
+        spaceBetween={15}
+        breakpoints={{
+          320: { slidesPerView: 2 },
+          640: { slidesPerView: 3 },
+          1024: { slidesPerView: 5 },
+        }}
+        className="w-full  mx-auto"
+      >
+        {images.map((img, index) => (
+          <SwiperSlide key={index} className="flex justify-center">
+            <img
+              src={img}
+              alt={`slider-${index}`}
+              className="rounded-[110px] object-cover mt-7  w-[170px] h-[255px]"
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+
+    
     </div>
-       <div className="images absolute top-[100px]   flex  overflow-hidden ">
-          <Carousel 
-          value={images}
-          circular
-          // showNavigators={true}
-          // showIndicators={true}
-          numScroll={1} 
-          numVisible={5}
-          autoplayInterval={3000}
-          itemTemplate={(item)=>(
-            <>
-            <img src={item}  alt='image' className='pl-4 rounded-[93px] xl:h-[] h-[200px] w-[200px] object-cover'/>
-            </>
-          )}
-          />
-          </div>
-        </div>
-  
+  );
+};
 
-    </>
-  )
-}
-
-export default Slider
+export default Slider;
